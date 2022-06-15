e6c6a89a78fac2e84ec12407f9cdde2b13e04166
Tahir Ahmed <01tahirahmed@gmail.com>
2022-06-15 16:57:13 +0530

    project submit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// variable declaration
+const downloadForApple = document.querySelector("#download-apple-btn");
+const downloadForAndroid = document.querySelector("#download-android-btn");
+
+downloadForApple.addEventListener("click", function(){
+    alert("The download button is not working!\nWe will fixed it soon!🤞");
+})
+
+downloadForAndroid.addEventListener("click", function(){
+    alert("The download button is not working!\nWe will fixed it soon!🤞");
+})
